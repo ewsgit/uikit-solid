ee9cd8e828ba2a95114b840442c632843f9e029e
ewsgit <ewsgit@ewsgit.uk>
2026-06-09 18:56:50 +0100

    complete split button

--- src/components/splitButtons/UKSplitButton.tsx
+++ src/components/splitButtons/UKSplitButton.tsx
@@ -7,6 +7,7 @@ import UKMenu, { type MenuItem } from "../menu/UKMenu";
 import type { SplitButtonColor } from "./lib/color";
 import type { SplitButtonSize } from "./lib/size";
 import styles from "./UKSplitButton.module.scss";
+import clsx from "clsx";
 
 const UKSplitButton: Component<{
   class?: string;
@@ -15,28 +16,33 @@ const UKSplitButton: Component<{
   size?: SplitButtonSize;
   items: (MenuItem | undefined)[];
 }> = (props) => {
-  const [dropdownSelected, setDropdownSelected] = createSignal<{x: number, y: number, align: "right", minWidth: number} | false>(false);
+  const [dropdownSelected, setDropdownSelected] = createSignal<{ x: number; y: number; align: "right"; minWidth: number } | false>(false);
 
   return (
-    <UKButtonGroup connected size={props.size || "s"} class={styles.root}>
-      <UKButton class={styles.textButton} size={props.size || "s"} color={props.color} onClick={() => {
-        props.items.find(item => item?.type === "button")?.onClick();
-      }}>
-        {props.items.find(item => item?.type === "button")?.label}
+    <div class={styles.root}>
+      <UKButton
+        class={styles.textButton}
+        size={props.size || "s"}
+        color={props.color}
+        onClick={() => {
+          props.items.find((item) => item?.type === "button")?.onClick();
+        }}
+      >
+        {props.items.find((item) => item?.type === "button")?.label}
       </UKButton>
       <UKIconButton
-        class={styles.iconButton}
+        class={clsx(styles.iconButton, dropdownSelected() !== false && styles.iconButtonDropdownSelected)}
         color={props.color !== "elevated" ? props.color : "filled"}
         icon={STAT_MINUS_1_ICON}
         size={props.size || "s"}
         alt="alt"
         onClick={(event) => {
-          const br = event.currentTarget.parentElement!.getBoundingClientRect()
+          const br = event.currentTarget.parentElement!.getBoundingClientRect();
           setDropdownSelected({ x: br.x, y: br.bottom, align: "right", minWidth: br.width });
         }}
       />
       <UKMenu vibrant showMenu={dropdownSelected} closeMenu={() => setDropdownSelected(false)} items={props.items}></UKMenu>
-    </UKButtonGroup>
+    </div>
   );
 };
 
